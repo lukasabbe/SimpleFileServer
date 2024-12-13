566d018183eed508c2b02625f379eb709ce1e8db
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-12-13 11:49:59 +0100

    added stats

--- index.js
+++ index.js
@@ -43,6 +43,22 @@ app.post('/upload', (req, res) => {
 app.get('/download/:fileName', (req, res) => {
     let fileName = req.params.fileName;
     if(fs.existsSync("./data/"+fileName)) {
+        if(!fs.existsSync("./data/data.json")){
+            fs.writeFileSync("./data/data.json", JSON.stringify([{fileName:fileName, downloads:1}]));
+        }else{
+            let data = JSON.parse(fs.readFileSync("./data/data.json"));
+            let found = false;
+            data.forEach(file => {
+                if(file.fileName == fileName) {
+                    file.downloads++;
+                    found = true;
+                }
+            });
+            if(!found) {
+                data.push({fileName:fileName, downloads:1});
+            }
+            fs.writeFileSync("./data/data.json", JSON.stringify(data));
+        }
         return res.download("./data/"+fileName, fileName);
     }
     return res.status(404).send("File not found");
@@ -53,14 +69,19 @@ app.get('/list', (req, res) => {
         if(err) {
             return res.status(500).send(err);
         }
+        if(!fs.existsSync("./data/data.json")){
+            fs.writeFileSync("./data/data.json", JSON.stringify([]));
+        }
         let filesObjs = [];
+        const data = fs.readFileSync("./data/data.json");
+        const dataJson = JSON.parse(data);
         files.forEach(file => {
-            filesObjs.push({"fileName":file, "link":"https://download.lukasabbe.com/download/"+file});
+            const downloads = dataJson.find(f => f.fileName == file)
+            filesObjs.push({"fileName":file, "link":"https://download.lukasabbe.com/download/"+file, "downloads": downloads ? downloads.downloads : 0});
         });
         res.json(filesObjs);
     });
 });
-
 
 app.listen(3010, () => {
     console.log('Server is running on port 3000');
